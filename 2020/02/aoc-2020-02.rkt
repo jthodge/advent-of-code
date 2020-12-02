28130947c0784c4cf5eb87e52f965b26fb2ffc9c
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-12-02 18:50:40 -0500

    day 02 part 02

--- 2020/02/aoc-2020-02.rkt
+++ 2020/02/aoc-2020-02.rkt
@@ -36,7 +36,7 @@
            password)
      (values min max c password)]))
 
-(define (password-valid? line)
+(define (password-valid?/old-policy line)
   (define-values (min max c password)
     (read-password-line line))
   (define cnt
@@ -47,5 +47,33 @@
        (<= cnt max)))
 
 (for/sum ([line (in-list lines)])
-  (if (password-valid? line) 1 0))
+  (if (password-valid?/old-policy line) 1 0))
 ; 500
+
+;While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+;
+;The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+;
+;Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+;
+;Given the same example list from above:
+;
+;1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+;1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+;2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+;How many passwords are valid according to the new interpretation of the policies?
+
+(define (password-valid?/new-policy line)
+  (define-values (min max c password)
+    (read-password-line line))
+  (define (reference position)
+    (define index (sub1 position))
+    (and (< index (string-length password))
+         (char=? c (string-ref password index))))
+  (if (reference min)
+      (not (reference max))
+      (reference max)))
+
+(for/sum ([line (in-list lines)])
+  (if (password-valid?/new-policy line) 1 0))
+; 313
